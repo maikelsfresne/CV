297e1a59b6f7166371e1c692195134ea3bb6ee9c
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-03-28 21:15:20 +0100

    3

--- ed_cv/src/clases/Clase_normal.java
+++ ed_cv/src/clases/Clase_normal.java
@@ -3,6 +3,6 @@ package clases;
 public class Clase_normal {
 	public static void main(String[] args) {
 		System.out.println("P1 y P2 podran trabajar sobre esta misma clase y confirmar sus cambios a"
-				+ " nivel local y en el repositorio remoto.");
+				+ " nivel local y en el repositorio remoto");
 	}
 }
