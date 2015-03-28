9df05a6a03bdc6e509c891b390bd0b93f0c8dcac
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-03-28 20:54:06 +0100

    2

--- ed_cv/src/clases/P1.java
+++ ed_cv/src/clases/P1.java
@@ -2,6 +2,6 @@ package clases;
 
 public class P1 {
 	public static void main(String[] args) {
-		System.out.println("trabajo exclusivo de P1, por ejemplo para crear nuevas funcionalidades del programa");
+		System.out.println("Trabajo exclusivo de P1, por ejemplo para crear nuevas funcionalidades del programa");
 	}
 }
