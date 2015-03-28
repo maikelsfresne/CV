63f68a70f978b12853a24eff9ddca4aca6f1e1e9
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-03-28 20:51:22 +0100

    commit 2

---
+++ ed_cv/src/clases/P1.java
@@ -0,0 +1,7 @@
+package clases;
+
+public class P1 {
+	public static void main(String[] args) {
+		System.out.println("trabajo exclusivo de P1, por ejemplo para crear nuevas funcionalidades del programa");
+	}
+}
